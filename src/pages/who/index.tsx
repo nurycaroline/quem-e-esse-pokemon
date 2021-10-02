0063ac9616516302a9932015672fbfd2ccce0c34
Nurielly Caroline Brizola <nurycaroline@gmail.com>
2021-10-02 19:04:37 -0300

    teste revalidade 3

--- src/pages/who/index.tsx
+++ src/pages/who/index.tsx
@@ -57,6 +57,6 @@ export const getStaticProps: GetStaticProps = async () => {
     props: {
       pokemon,
     },
-    revalidate: true, //60 * 60 * 24, //24 hours
+    revalidate: 1, //60 * 60 * 24, //24 hours
   };
 };
